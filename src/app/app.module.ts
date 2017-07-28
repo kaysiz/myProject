0b504c4144df861f76c8983a3a62fff72d958a77
kudakwashe siziva <ksiziva@gmail.com>
2017-07-28 21:45:53 +0200

    ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
